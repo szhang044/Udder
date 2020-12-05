5c0755233dbb89d48e06ee9ec754e564a36cb088
Zhang <bla@gmail.com>
2020-12-05 23:49:48 +0800

    Add Label

---
+++ Udder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Zhang Shi Hong on 05/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
